9d28df0a29d414232d0f5324fd30e68ba2a75b3f
rodrigo mardones <rignmardones@falabella.cl>
2022-05-05 10:41:04 -0400

    add: setup

---
+++ makefile
@@ -0,0 +1,4 @@
+compile:
+	gcc src/main.c -o instance.out
+run:
+	./instance.out
---
+++ src/main.c
@@ -0,0 +1,6 @@
+#include "stdio.h"
+
+int main(int args,char* argsv[]){
+    printf("hello friend from c \n");
+    return 0;
+}
