3b276d986ca0848e8f619cdd8d9a416bd349f504
Felipe Améstica <felipe.amestica@usach.cl>
2022-05-07 15:17:10 -0400

    falta leer los args

--- src/lab1.c
+++ src/lab1.c
@@ -3,26 +3,35 @@
 #include <string.h>
 #include <ctype.h>
 #include <math.h>
+#include <unistd.h> 
 #include "leerArchivo.h"
 
-void calculateDiscVisibility(char*** matrix, int initFilas, int endFilas, int ncols, int vis, char* salida);
+int** setRange(int deep,int discs);
+void writeDiscs(char*** matrix, int** ranges, int nfilas,int ncols,  int discs, char* salida);
 
 int main(int argc,char* argv[]){
 
+
+    //Variables locales
     char*** matrix = NULL;
+    int** rangos = NULL;
     char *filename, *outputfile;
-    int nfilas, ncolumnas;
+    int nfilas, ncolumnas,ancho,discos;
 
     filename = "./src/uvplaneco1.csv";
     outputfile = "./src/propiedades.txt";
     nfilas = 0;
     ncolumnas = 6; 
+    ancho = 100;
+    discos = 4;
 
     nfilas = rowNumber(filename);	
     matrix = setMatrix(matrix, nfilas, ncolumnas);
     matrix = fillMatrix(filename, matrix);
     matrix = setDisc(matrix, nfilas);
-    calculateDiscVisibility(matrix, 1000,2000,ncolumnas,1000,outputfile);
+    rangos = setRange(ancho,discos);
+    unlink(outputfile);
+    writeDiscs(matrix, rangos, nfilas,ncolumnas, discos, outputfile);
 	return 0;
 }
 
--- src/vis.c
+++ src/vis.c
@@ -4,42 +4,115 @@
 #include <ctype.h>
 #include <math.h>
 
-void calculateDiscVisibility(char*** matrix, int initFilas, int endFilas, int ncols, int vis, char* salida){
+//Establecer rango de distancia de visibilidad de cada disco de acuedo al ancho
+int** setRange(int deep, int discs){
+
+    int** ranges;
+	ranges = (int**) malloc(sizeof(int*)*discs);
+	for(int i=0;i<discs;i++){	
+		ranges[i] = (int*) malloc(sizeof(int)*2);
+	}
+
+	int i;
+    int start = 0;
+	int end = deep - 1;
+
+	for(i=0;i<discs;i++){	
+		ranges[i][0] = start;
+		ranges[i][1] = end;
+		if(i == discs - 1){
+			ranges[i][1] = 0;
+		}
+		start = end + 1;
+		end = end + deep;
+	}
+
+	return ranges;
+}
+
+//Calcular los datos de cada disco
+void calculateDiscVisibility(char*** matrix, int nfilas, int initFilas, int endFilas, int nvis, int numdisco, char* salida){
 	double mediaReal = 0;
 	double mediaImaginaria = 0;
 	double ruidoTotal = 0;
 	double potencia = 0;
-	for(int i = initFilas; i < endFilas; i++){
-		double realPotencia = strtod(matrix[i][2], (char**) NULL);
-		double imaginarioPotencia = strtod(matrix[i][3], (char**) NULL);
-		double expresion = pow(realPotencia,2) + sqrt(pow(imaginarioPotencia,2));
-		potencia = potencia + expresion;
-		for(int j = 0; j < ncols;j++){
-			if(j == 2){
-				double real = strtod(matrix[i][j], (char **)NULL);
+
+	for(int i=0; i < nfilas; i++){
+		//Coord visibilidad
+		double u = strtod(matrix[i][0], (char **)NULL);
+		double v = strtod(matrix[i][1], (char **)NULL);
+		//Parte real
+		double real = strtod(matrix[i][2], (char **)NULL);
+		//Pater imaginaria
+		double imaginario = strtod(matrix[i][3], (char **)NULL);
+		//Ruido
+		double ruido = strtod(matrix[i][4], (char **)NULL);
+		//Distancia
+		double distancia = strtod(matrix[i][5], (char **)NULL);
+		//Rango de disco final (x hasta el final)
+		if(endFilas == 0)
+		{
+			if(distancia >= initFilas){
+				double expresion = pow(real,2) + sqrt(pow(imaginario,2));
+				potencia = potencia + expresion;
 				mediaReal = mediaReal + real;
-			}
-			if(j == 3){
-				double imaginario = strtod(matrix[i][j], (char **)NULL);
 				mediaImaginaria = mediaImaginaria + imaginario;
+				ruidoTotal = ruidoTotal + ruido;
 			}
-			if(j == 4){
-				double ruido = strtod(matrix[i][j], (char**)NULL);
+		}
+		//Rango de disco finito (x a y)
+		else
+		{
+			if(distancia >= initFilas && distancia <= endFilas){
+				double expresion = pow(real,2) + sqrt(pow(imaginario,2));
+				potencia = potencia + expresion;
+				mediaReal = mediaReal + real;
+				mediaImaginaria = mediaImaginaria + imaginario;
 				ruidoTotal = ruidoTotal + ruido;
 			}
 		}
 	}
-	mediaReal = (mediaReal/vis);
-	mediaImaginaria = (mediaImaginaria/vis);
 
-    FILE* f = fopen(salida, "w");
-	fprintf(f,"Disco %i:\n",1);
+	mediaReal = (mediaReal/nvis);
+	mediaImaginaria = (mediaImaginaria/nvis);
+
+
+    FILE* f = fopen(salida, "a");
+	fprintf(f,"Disco %i:\n",numdisco);
     fprintf(f,"Media real: %f \n", mediaReal);
 	fprintf(f,"Media imaginaria: %f \n", mediaImaginaria);
 	fprintf(f,"Potencia: %f \n", potencia);
-	fprintf(f,"Ruido total: %f \n",ruidoTotal);
+	fprintf(f,"Ruido total: %f \n \n",ruidoTotal);
     fclose(f);
 }
 
+//Calcular el n° de distancias de visibilidad en cada disco
+int visNumber(char*** matrix, int nfilas, int initFilas, int endFilas){	
+	int i,j,numbervis;
+	numbervis = 0;
+	for(i=0; i<nfilas; i++){
+		int vis = atoi(matrix[i][5]);
+		if (endFilas == 0){
+				if(vis >= initFilas){
+			numbervis++;
+			}
+		}else{
+			if(vis >= initFilas && vis <= endFilas){
+				numbervis++;
+			}
+		}
+	}
+	return numbervis;
+}
+
+//Escribir la info correspondiente a cada disco
+void writeDiscs(char*** matrix, int** ranges, int nfilas,int ncols,  int discs, char* salida){
+	int i,j,nvis;
+	for(i=0; i<discs; i++){
+			nvis = visNumber(matrix, nfilas, ranges[i][0], ranges[i][1]);
+			calculateDiscVisibility(matrix, nfilas, ranges[i][0], ranges[i][1], nvis,(i + 1),salida);
+	}
+
+}
 
 
